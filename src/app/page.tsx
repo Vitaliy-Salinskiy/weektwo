529eb6fefbf1e6dfdab5f0ea5071df28fa18d57f
Vitaliy-Salinskiy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-03-29 05:56:59 +0100

    getting url through headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import { headers } from "next/headers";
+
 import BigCalendar from "@/components/shared/BigCalendar";
 import { ExtendedSlot } from "@/interfaces";
 
 export default async function Home() {
-  const slotsResponse = await fetch(
-    `https://weektwo-6reh40v5o-vitaliys-projects-e60c5620.vercel.app/api/slots`
-  );
+  const headersList = headers();
+  const fullUrl = headersList.get("referer") || "";
+
+  const slotsResponse = await fetch(`${fullUrl}api/slots`);
   const slots: Partial<ExtendedSlot[]> = await slotsResponse.json();
 
   return (
     <main className="flex">
-      <BigCalendar slots={[]} />
+      <BigCalendar slots={slots} />
     </main>
   );
 }
