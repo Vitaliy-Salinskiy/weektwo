814e495c1618481ebe85fd2757968ea4fd6d00ec
Vitaliy-Salinskiy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-03-29 05:50:11 +0100

    last hope commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import BigCalendar from "@/components/shared/BigCalendar";
-// import { ExtendedSlot } from "@/interfaces";
+import { ExtendedSlot } from "@/interfaces";
 
 export default async function Home() {
-  // const slotsResponse = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/slots`
-  // );
-  // const slots: Partial<ExtendedSlot[]> = await slotsResponse.json();
+  const slotsResponse = await fetch(
+    `https://weektwo-6reh40v5o-vitaliys-projects-e60c5620.vercel.app/api/slots`
+  );
+  const slots: Partial<ExtendedSlot[]> = await slotsResponse.json();
 
   return (
     <main className="flex">
